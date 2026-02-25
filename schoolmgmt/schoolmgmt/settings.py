f7cc1908e013ae8d4eed7d80a9000dfdf16acf77
Phoenix-legend1 <63068100+Phoenix-legend1@users.noreply.github.com>
2026-02-25 11:34:54 +0530

    Fix: add default secret key and fix admin page module

--- schoolmgmt/schoolmgmt/settings.py
+++ schoolmgmt/schoolmgmt/settings.py
@@ -17,12 +17,14 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = config('SECRET_KEY')
+# This uses the .env value if it exists, otherwise it uses a dummy key for testing
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-test-key-only')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
